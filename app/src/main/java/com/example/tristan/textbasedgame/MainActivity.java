8556a152fdfcc635623b47e4e387d605035904f1
tjcdavy <tjcdavy@utas.edu.au>
2017-05-07 00:16:18 +1000

    Added new xml, added string thing and started some backend, maybe...

--- app/src/main/java/com/example/tristan/textbasedgame/MainActivity.java
+++ app/src/main/java/com/example/tristan/textbasedgame/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.tristan.textbasedgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goButton(){
+        String tempname;
+        //EditText texty = findViewById(R.id.name);
     }
 }
