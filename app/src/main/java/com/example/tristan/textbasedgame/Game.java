7f7a279a1e45e085aad21ce6568b6ded003f133a
tjcdavy <tjcdavy@utas.edu.au>
2017-05-08 00:38:41 +1000

    Testing out the game screen

---
+++ app/src/main/java/com/example/tristan/textbasedgame/Game.java
@@ -0,0 +1,20 @@
+package com.example.tristan.textbasedgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Tristan on 7/05/2017.
+ */
+
+public class Game extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    }
+
+
+
+}
