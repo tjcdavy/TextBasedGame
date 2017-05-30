6a51f3e3e7d5a9e0c258b4df3e26ab26f41c71f7
tjcdavy <tjcdavy@utas.edu.au>
2017-05-30 21:59:43 +1000

    Implemented Buttons on main, fixed formatting on game page

--- app/src/main/java/com/example/tristan/textbasedgame/MainActivity.java
+++ app/src/main/java/com/example/tristan/textbasedgame/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.tristan.textbasedgame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +19,33 @@ public class MainActivity extends AppCompatActivity {
 
         Button nameButton = (Button) findViewById(R.id.namebutton);
         Button goButton = (Button) findViewById(R.id.gobutton);
-        
+
+        nameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText nameField = (EditText) findViewById(R.id.name);
+                name = nameField.getText().toString();
+                switchLayout();
+
+            }
+        });
+
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Game.class);
+                intent.putExtra("name", name);
+                startActivity(intent);
+            }
+        });
     }
 
+    public void switchLayout(){
+        TextView newText = (TextView) findViewById(R.id.intro_speech);
+        String pt1 = getResources().getString(R.string.intro);
+        String pt2 = getResources().getString(R.string.intro2);
+        String finalText = new String("Well then, " + name + ", "+ pt1 + "\n" + pt2);
+        newText.setText(finalText);
+    }
 
 }
